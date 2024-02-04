2ad9275871da0c0d530a14dcf2b43657c822f918
kaziarman23 <kaziarman@proton.me>
2024-02-04 11:56:10 +0600

    project-accomplished-with-new-style

---
+++ JS/index.js
@@ -0,0 +1,26 @@
+const display = document.querySelector("#inputBox")
+let buttons = document.querySelectorAll("button")
+
+const btn_Array = Array.from(buttons)
+let string = ""
+btn_Array.forEach(function(btn){
+    btn.addEventListener("click",function(e){
+
+        if(e.target.innerHTML === 'DEL'){
+            string = string.substring(0, string.length-1)
+            display.value = string
+        }
+        else if(e.target.innerHTML === 'AC'){
+            string = "";
+            display.value = string
+        }
+        else if(e.target.innerHTML === '='){
+            string = eval(string);
+            display.value = string;
+        }
+        else{
+        string += e.target.innerHTML
+        display.value = string
+        }
+    })
+})
